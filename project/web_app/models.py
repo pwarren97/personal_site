a0f58be50fa065fc087062aeabba02256579389c
Peyton Warren <pwarren97@gmail.com>
2019-06-15 14:58:33 -0500

    models complete

--- project/web_app/models.py
+++ project/web_app/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Admin(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class BlogEntry(models.Model):
+    pub_date = models.DateField()
+    edit_date = models.DateField()
+    title = models.CharField(max_length=40)
+    entry = models.CharField(max_length=5000)
+    
+
+    def __str__(self):
+        return self.tile
