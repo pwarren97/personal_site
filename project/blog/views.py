45ac1b3960e02a25809b4e59800aad2f951028ee
Peyton Warren <pwarren97@gmail.com>
2019-07-02 14:56:41 -0500

    switched to C#:

--- project/blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("<html><body><h1>adf</h1></body></html>")
