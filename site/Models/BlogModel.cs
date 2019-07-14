db8069df41d3b99b2ee9f0469648e26b78c9100a
Peyton Warren <pwarren97@gmail.com>
2019-07-13 22:27:00 -0500

    Added BlogModel

---
+++ site/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site.Models
+{
+    public class BlogModel
+    {
+
+    }
+}
