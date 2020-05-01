a5f68e5c463a3158dd41ec2f79e1636b4716eb3d
Peyton Warren <pwarren97@gmail.com>
2020-05-01 18:24:16 -0500

    Updates to layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Warren Freelance Software Development' });
+  res.render('index', { title: 'Warren Freelance Software Development LLC' });
 });
 
 module.exports = router;
