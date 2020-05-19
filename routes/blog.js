960aef199686e0d424e70b3e8160827e2b1842e0
Peyton <pwarren97@gmail.com>
2020-05-19 16:09:23 -0500

    Nav button text now center-aligned

--- routes/blog.js
+++ routes/blog.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET blog listing. */
 router.get('/', function(req, res, next) {
   // pull entries from database
-  res.render('entrylist', { entries: [{title: 'Entry 1', data: 'The int.'}] });
+  res.render('entrylist', { entries: [{title: 'Entry 1', data: 'The int.'}, {title: 'Entry 2', data: 'The int.'}] });
 });
 
 module.exports = router;
