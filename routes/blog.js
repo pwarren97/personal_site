3cb32d5b5310aae6f56f8ee81936297a6fb7f5a9
Peyton <pwarren97@gmail.com>
2020-05-23 14:14:56 -0500

    updated entry.pug to have parameters passed throuh

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+dbRepresentation = { entries: [{title: 'Entry 1', id: 1, data: 'The int.', date: '2019-01-01'}, {title: 'Entry 2', id: 2, data: 'The int.', date: '2019-01-01'}] };
+
 /* GET blog listing. */
 router.get('/', function(req, res, next) {
   // pull entries from database
-  res.render('entrylist', { entries: [{title: 'Entry 1', data: 'The int.', date: '2019-01-01'}, {title: 'Entry 2', data: 'The int.', date: '2019-01-01'}] });
+  res.render('entrylist', dbRepresentation);
+});
+
+router.get('/entry/:id', function(req, res, next) {
+  res.render('entry', { data: req.params.id });
 });
 
 module.exports = router;
