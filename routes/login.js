ae4db1f312e4d45e90b682d950de38fca39bff46
Peyton <pwarren97@gmail.com>
2020-06-09 12:36:07 -0500

    Refactored the Cookie options and the correct password hash to the top of the page; Added log out operation; Added routes for the rest of the references on the admin-panel.pug page

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 
+var hashedCorrectPassword = "7c942e7f59dcc59d131134a97d39754a428f829c5a6e318be269cce4571cd83c";
+
 var hour = 3600000;
 var maxAge = 24 * hour;
+var loginCookieOptions = {
+  maxAge: maxAge,
+  sameSite: true
+};
+
 
 /* GET login pages */
+
+// Login Page
 router.post('/', function(req, res, next) {
   // Create a hash of the password from the user
   var hash = crypto.createHash('sha256');
@@ -14,19 +23,15 @@ router.post('/', function(req, res, next) {
   hashedPassword = hashedPassword.toString('hex');
 
   // check to see if the username and password are correct
-  if (req.body.username === "peyton" && hashedPassword === "7c942e7f59dcc59d131134a97d39754a428f829c5a6e318be269cce4571cd83c") {
+  if (req.body.username === "peyton" && hashedPassword === hashedCorrectPassword) {
     // Assign cookie
-    res.cookie('user', 'admin', {
-      maxAge: maxAge,
-      sameSite: true
-    });
+    res.cookie('user', 'admin', loginCookieOptions);
     res.redirect('/login/admin-panel');
   }
   else {
     res.render('login');
   }
 });
-
 
 router.get('/', function(req, res, next) {
   // Return something if logged in, otherwise the login page
@@ -38,6 +43,18 @@ router.get('/', function(req, res, next) {
   }
 });
 
+// Log Out Operation
+router.get('/log-out', function(req, res, next) {
+  if (req.cookies.user === 'admin') {
+    res.clearCookie('user');
+    res.redirect('/login');
+  }
+  else {
+    throw new Error('404');
+  }
+});
+
+// Admin Panel
 router.get('/admin-panel', function(req, res) {
   // return admin panel only if there is a proper cookie
   if (req.cookies.user === 'admin') {
@@ -46,6 +63,26 @@ router.get('/admin-panel', function(req, res) {
   else {
     throw new Error('404');
   }
+});
+
+// Add a Blog Entry
+router.get('/add-blog-entry', function(req, res) {
+  
+});
+
+// Remove a Blog Entry
+router.get('/remove-blog-entry', function(req, res) {
+
+});
+
+// Add a Past Project
+router.get('/remove-blog-entry', function(req, res) {
+
+});
+
+// Remove a Past Project
+router.get('/remove-blog-entry', function(req, res) {
+
 });
 
 module.exports = router;
