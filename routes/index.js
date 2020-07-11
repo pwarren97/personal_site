a0f033d1f048916acfb5289d90cbbbb7d73dc5d3
Peyton <pwarren97@gmail.com>
2020-07-10 20:32:37 -0500

    Added info to homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Warren Freelance Software Development' });
+  res.render('index', { title: 'Peyton Warren' });
 });
 
 module.exports = router;
